bdea1c037b9d8425348c154d682f26b6dc39f0a3
abrarasif11 <faasif929@gmail.com>
2025-10-21 11:53:13 +0600

    handle modal state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import ModalForm from "./Components/ModalForm/ModalForm";
 import Navbar from "./Components/Navbar/Navbar";
 import Table from "./Components/Table/Table";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMode, setModalMode] = useState("add");
+
+  const handleOpen = (mode) => {
+    setIsOpen(true);
+  };
+
+  const handleSubmit = () => {
+    if (modalMode === "add") {
+      console.log("Modal Mode Added");
+    } else {
+      console.log("Modal Mode Edited");
+    }
+  };
   return (
     <>
-      <Navbar />
+      <Navbar onOpen={() => handleOpen("add")} />
       <Table />
+      <ModalForm isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </>
   );
 }
